5182c992649502d81c935a126ee12ee9acfc23a1
Phil Oye <philoye@philoye.com>
2009-07-21 21:19:00 +1000

    Getting closer...

--- lib/twitter.rb
+++ lib/twitter.rb
@@ -13,5 +13,11 @@ class Twitter
    options.merge!({ :basic_auth => @auth} )
    self.class.get("/statuses/#{which}_timeline.json", options)
   end
+
+  def filtered_tweets(whitelist=nil,blacklist=nil)
+    timeline.reject do |tweet|
+      tweet['text'][0] == 64 # or tweet['text'].downcase.include? "definitely"
+    end
+  end
   
 end
