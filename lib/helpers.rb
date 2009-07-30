15d1e2c1e26c0bfe25844eb8ac03cb100b555fd3
Phil Oye <philoye@philoye.com>
2009-07-30 22:45:57 +1000

    Hot damn, we have video

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -21,8 +21,15 @@ def flickr_square(photo)
   %(<img src="#{flickr_src(photo, "s")}" width="75" height="75" title="#{photo['title']}">)
 end
 def flickr_embed_code(video,desired_width)
-  info_box = "true"
-  %w(<object type="application/x-shockwave-flash" width="#{video['width']}" height="#{video['height']}" data="#{video['url']}"  classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"> <param name="flashvars" value="flickr_show_info_box=#{info_box}"></param> <param name="movie" value="#{video['url']}"></param><param name="bgcolor" value="#000000"></param><param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="#{video['url']}" bgcolor="#000000" allowfullscreen="true" flashvars="flickr_show_info_box=#{info_box}" height="#{video['height']}" width="#{video['width']}"></embed></object>)
+  info_box = "false"
+  width = video['width']
+  height = video['height']
+  if (desired_width < width)
+    height = desired_width.to_i * height.to_i / width.to_i
+    width = desired_width
+  end
+  
+  %(<object type="application/x-shockwave-flash" width="#{width}" height="#{height}" data="#{video['source']}"  classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"> <param name="flashvars" value="flickr_show_info_box=#{info_box}"></param> <param name="movie" value="#{video['source']}"></param><param name="bgcolor" value="#000000"></param><param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="#{video['source']}" bgcolor="#000000" allowfullscreen="true" flashvars="flickr_show_info_box=#{info_box}" height="#{height}" width="#{width}"></embed></object>)
 end
 def photo_path(photo)
   user = user_from_nsid(photo['owner'])
