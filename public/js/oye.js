c3608585f418f75e6b054dfb0d3915c73b8fa92b
Phil Oye <philoye@philoye.com>
2009-08-04 20:29:28 +1000

    JS to ensure that the container is a multiple of the thumbnail width

--- public/js/oye.js
+++ public/js/oye.js
@@ -1,28 +1,18 @@
 $(document).ready(function() {
 
-  target = $(".container");
+  function resizeContainer() {
+    var window_width = $(window).width();
+    var num_columns = Math.floor((window_width - 50) / 79);
+    var new_width = (num_columns * 79);
+    $(".container").width(new_width);
+  }
+
+  var resizeTimer = null;
+  $(window).resize(function() {
+      if (resizeTimer) clearTimeout(resizeTimer);
+      resizeTimer = setTimeout(resizeContainer, 100);
+  });
   
-  current_width = target.width();
-  num_columns = Math.floor((current_width - 4) / 79);
-  new_width = (num_columns * 79);
-  
-  target.width(new_width);
+  resizeContainer();
   
 });
-
-
-// var resizeTimer = null;
-// $(window).bind('resize', function() {
-//     if (resizeTimer) clearTimeout(resizeTimer);
-//     resizeTimer = setTimeout(doSomething, 100);
-// });
-
-// function resizeStuff() {
-//  //Time consuming resize stuff here
-// }
-// var TO = false;
-// $(window).resize(function(){
-//  if(TO !== false)
-//     clearTimeout(TO);
-//  TO = setTimeout(resizeStuff, 200); //200 is time in miliseconds
-// });
